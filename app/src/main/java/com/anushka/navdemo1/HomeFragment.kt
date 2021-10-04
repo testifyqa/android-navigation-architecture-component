1979346b6fd1470a3b475940328b90a3adfd318b
Thomas Knee <tom@testifyqa.com>
2021-10-04 13:00:32 +0100

    add navigation action

--- app/src/main/java/com/anushka/navdemo1/HomeFragment.kt
+++ app/src/main/java/com/anushka/navdemo1/HomeFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.anushka.navdemo1.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -18,6 +20,9 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding.button.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment)
+        }
         return binding.root
     }
 }
